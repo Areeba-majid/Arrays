bdff2636640b675914b9c84bf8098d4d8cd7134a
Areeba Riaz <160718011+Areeba-majid@users.noreply.github.com>
2024-04-01 00:55:44 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+
+let biryani = ["rice","chicken","spices","oil","onion","tomato"];
+//.push
+biryani.push("yougart")
+
+console.log(biryani)
+
+//.pop
+
+biryani.pop()
+
+console.log(biryani)
+
+//concat
+
+biryani = biryani.concat(["chilli" , "mint"])
+
+console.log(biryani)
+
+
+
+
